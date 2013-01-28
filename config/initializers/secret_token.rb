89da598b58954039fca4209541b5348a54e54bd6
Seungjae <seungjaesong@hotmail.com>
2013-01-29 01:16:29 +0900

    Initial commit MenuBoard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MenuBoard::Application.config.secret_token = '4487e7d958e7e7b6b26366232e39119787598223690221bfd2434d78642309f45b7ee949660c5b2d76a63775fd38e8dddc5b13e50d86bd88535e100447e264b8'
