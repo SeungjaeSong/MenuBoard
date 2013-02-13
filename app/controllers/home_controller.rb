96354b6ae6b102b59333517153a8ebbe034f49d0
jonghyunLim <jhlim8182@gmail.com>
2013-02-14 01:59:25 +0900

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,11 @@ class HomeController < ApplicationController
   def index
   	@width = params[:width]
     @height = params[:height]
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @restaurants }
+    end
   end
   def about
   end
